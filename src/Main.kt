106090498fcb85fa4dad8ba0f458e1738d1723d0
Ehsan Fallahi <ehsanfallahi@gmail.com>
2020-08-27 14:36:09 +0430

    also function

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,19 @@
 
 fun main(){
+
+    letScopeFunction()
+    alsoScopeFunction()
+}
+
+fun alsoScopeFunction() {
+    var stringList= mutableListOf<String>("a","b","c","ab","bc","abc","abcd")
+    var resultList=stringList.map { it.length }.filter { it>2 }
+    println("without elso:$resultList")
+
+    stringList.map { it.length }.also { println("with also:$it") }
+}
+
+fun letScopeFunction() {
     var myString:String?=null
 
     var length=if (myString?.length!=null ){
